44cd6cbdc97d7d8bdb2ae241fe6c90fa4a684940
alexandru-bambergher-hs <alexandru.bambergher@hootsuite.com>
2016-10-03 17:17:43 +0300

    Update output resource sample

--- ra_api.py
+++ ra_api.py
@@ -2,7 +2,18 @@
 """A basic (single function) API written using hug"""
 import hug
 
-@hug.get('/get')
+sample_output =  [
+   {
+      "status":"free",
+      "res":{
+         "staging":[
+            "user1:password123"
+         ]
+      }
+   }
+]
+
+@hug.get()
 def get(res, num:hug.types.number=1):
     """Get a number of resources of certain type!"""
-    return "Get {num} resource(s) of type {res}!".format(**locals())
+    return sample_output
