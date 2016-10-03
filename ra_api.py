51c86bac72e479cee48dd363d791ae122d923960
alexandru-bambergher-hs <alexandru.bambergher@hootsuite.com>
2016-10-03 18:02:04 +0300

    Use lock and unlock as get and post

--- ra_api.py
+++ ra_api.py
@@ -2,7 +2,7 @@
 """A basic (single function) API written using hug"""
 import hug
 
-sample_output =  [
+sample_output1 =  [
    {
       "status":"free",
       "res":{
@@ -13,7 +13,40 @@ sample_output =  [
    }
 ]
 
+sample_output2 =  [
+   {
+      "status":"busy",
+      "res":{
+         "production":[
+            "user3:password321"
+         ]
+      }
+   }
+]
+
+sample_output3 =  [
+   {
+      "status":"NA",
+      "res":{
+         "staging":[
+            "user2:password321",
+            "user1:password321"
+         ]
+      }
+   }
+]
+
 @hug.get()
-def get(res, num:hug.types.number=1):
-    """Get a number of resources of certain type!"""
-    return sample_output
+def lock(res, num:hug.types.number=1):
+    """Lock a number of resources of certain type!"""
+    if num == 3:
+        return sample_output3
+    elif num == 2:
+        return sample_output2
+    else:
+        return sample_output1
+
+@hug.post()
+def unlock(body):
+    """Unlock resource(s) in json format!"""
+    return body
