7a6c2ae7614e3836c046e9a2b2175b2617f7b38c
Alexandru Georgescu <alexandru.georgescu@hootsuite.com>
2016-10-04 14:46:48 +0300

    Updated the lock_credentials  and added the unlock_credentials function.

--- locktest.py
+++ locktest.py
@@ -1,25 +1,49 @@
-import threading
+from collections import defaultdict
 import json
 
-lock = threading.RLock()
-filename = "input_resources.json"
+# lock = threading.RLock()
+# filename = "input_resources.json"
+#
+# RESOURCE_JSON_FILE = 'input_resources.json'
+#
+# def get_json_data():
+#     """Load initial JSON data to the api on startup"""
+#     global JSON_DATA
+#     with open(RESOURCE_JSON_FILE, encoding='utf-8') as data_file:
+#         JSON_DATA = json.loads(data_file.read())
 
+def lock_credentials(JSON_DATA, user=None, medium = "staging"):
 
-def lock_credentials(medium = "staging"):
-    # TODO:
-    lock.acquire()
-    try:
-    # ... fetch data for first part from shared object
-        text = open(filename)
-        parsed_json = json.load(text)
+    if JSON_DATA[medium][user].get("status") != "busy":
+        # lock.acquire()
+        try:
+            JSON_DATA[medium][user]["status"] = "busy"
+            print("resource %s locked" % user)
+            print(JSON_DATA[medium])
 
-        for credential in parsed_json[medium]:
-            credentials = credential.split(":")
-            # print credentials
+        except Exception as e:
+            raise e
 
-    finally:
-        lock.release()
-    # return data
+        return True
+    else:
+        print("Couldn't acquire lock")
+        return False
 
+def unlock_credentials(JSON_DATA, user=None, medium = "staging"):
+    if JSON_DATA[medium][user].get("status") == "busy":
+        try:
+            JSON_DATA[medium][user]["status"] = "free"
+            # lock.release()
+        except Exception as e:
+            raise e
+        print("Resource %s unlocked", user)
+        return True
+    else:
+        print("Resource %s already free", user)
+        return False
 
-lock_credentials()
+# testing the JSON import
+# lock_credentials(JSON_DATA,user="user2")
+# lock_credentials(JSON_DATA,user="user2")
+# unlock_credentials(JSON_DATA, user="user2")
+# lock_credentials(JSON_DATA,user="user2")
