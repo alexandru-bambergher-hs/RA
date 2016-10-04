bb75f777208d089532537ec46f4df8bf15e3c2af
alexandru-bambergher-hs <alexandru.bambergher@hootsuite.com>
2016-10-04 18:43:59 +0300

    Update locking mechanism v1

--- locktest.py
+++ locktest.py
@@ -1,49 +1,77 @@
 from collections import defaultdict
-import json
 
-# lock = threading.RLock()
-# filename = "input_resources.json"
-#
-# RESOURCE_JSON_FILE = 'input_resources.json'
-#
-# def get_json_data():
+def lock_credentials(JSON_DATA, res_type = None, res_value = None):
+    if not JSON_DATA.get(res_type):
+        print("Resource type %s not defined" % res_type)
+        return {res_type:{res_value:{"status": "NA"}}}
+
+    if JSON_DATA[res_type].get(res_value):
+        if JSON_DATA[res_type][res_value].get("status") != "busy":
+            try:
+                JSON_DATA[res_type][res_value]["status"] = "busy"
+                print("Resource %s locked" % res_value)
+            except Exception as e:
+                print("Resource %s not found" % res_value)
+                return {res_type:{res_value:{"status": "NA"}}}
+            return {res_type:{res_value:JSON_DATA[res_type][res_value]}}
+        else:
+            print("Already locked for %s" % res_value)
+            return {res_type:{res_value:JSON_DATA[res_type][res_value]}}
+    else:
+        print("Resource %s not found" % res_value)
+        return {res_type:{res_value:{"status": "NA"}}}
+
+def unlock_credentials(JSON_DATA, res_type = None, res_value = None):
+    if not JSON_DATA.get(res_type):
+        print("Resource type %s not defined" % res_type)
+        return {res_type:{res_value:{"status": "NA"}}}
+
+    if JSON_DATA[res_type].get(res_value):
+        if JSON_DATA[res_type][res_value].get("status") != "free":
+            try:
+                JSON_DATA[res_type][res_value]["status"] = "free"
+                print("Resource %s unlocked" % res_value)
+            except Exception as e:
+                print("Resource %s not found" % res_value)
+                return {res_type:{res_value:{"status": "NA"}}}
+            return {res_type:{res_value:JSON_DATA[res_type][res_value]}}
+        else:
+            print("Resource %s already unlocked" % res_value)
+            return {res_type:{res_value:JSON_DATA[res_type][res_value]}}
+    else:
+        print("Resource %s not found" % res_value)
+        return {res_type:{res_value:{"status": "NA"}}}
+
+# #testing the JSON import
+# import json
+# res_value_JSON_FILE = 'input_resources.json'
+
+# def load_json_data():
 #     """Load initial JSON data to the api on startup"""
 #     global JSON_DATA
-#     with open(RESOURCE_JSON_FILE, encoding='utf-8') as data_file:
+#     with open(res_value_JSON_FILE, encoding='utf-8') as data_file:
 #         JSON_DATA = json.loads(data_file.read())
 
-def lock_credentials(JSON_DATA, user=None, medium = "staging"):
+# load_json_data()
 
-    if JSON_DATA[medium][user].get("status") != "busy":
-        # lock.acquire()
-        try:
-            JSON_DATA[medium][user]["status"] = "busy"
-            print("resource %s locked" % user)
-            print(JSON_DATA[medium])
+# lock_credentials(JSON_DATA, res_type="staging", res_value="empty")
+# print(lock_credentials(JSON_DATA, res_type="staging", res_value="user1"))
+# lock_credentials(JSON_DATA, res_type="staging", res_value="user2")
+# lock_credentials(JSON_DATA, res_type="production", res_value="user3")
+# lock_credentials(JSON_DATA, res_type="production", res_value="user3")
+# lock_credentials(JSON_DATA, res_type="production", res_value="user4")
+# lock_credentials(JSON_DATA, res_type="production")
+# lock_credentials(JSON_DATA)
 
-        except Exception as e:
-            raise e
+# print(JSON_DATA)
 
-        return True
-    else:
-        print("Couldn't acquire lock")
-        return False
+# unlock_credentials(JSON_DATA, res_type="staging", res_value="empty")
+# unlock_credentials(JSON_DATA, res_type="staging", res_value="user1")
+# unlock_credentials(JSON_DATA, res_type="production", res_value="user2")
+# unlock_credentials(JSON_DATA, res_type="production", res_value="user3")
+# unlock_credentials(JSON_DATA, res_type="production", res_value="user3")
+# unlock_credentials(JSON_DATA, res_type="production", res_value="user4")
+# unlock_credentials(JSON_DATA, res_type="staging")
+# unlock_credentials(JSON_DATA)
 
-def unlock_credentials(JSON_DATA, user=None, medium = "staging"):
-    if JSON_DATA[medium][user].get("status") == "busy":
-        try:
-            JSON_DATA[medium][user]["status"] = "free"
-            # lock.release()
-        except Exception as e:
-            raise e
-        print("Resource %s unlocked", user)
-        return True
-    else:
-        print("Resource %s already free", user)
-        return False
-
-# testing the JSON import
-# lock_credentials(JSON_DATA,user="user2")
-# lock_credentials(JSON_DATA,user="user2")
-# unlock_credentials(JSON_DATA, user="user2")
-# lock_credentials(JSON_DATA,user="user2")
+# print(JSON_DATA)
--- ra_api.py
+++ ra_api.py
@@ -1,44 +1,11 @@
 # filename: ra_api.py
 """A basic (single function) API written using hug"""
 import hug
+import json
 from locktest import *
 
 JSON_DATA = []
 RESOURCE_JSON_FILE = 'input_resources.json'
-
-sample_output1 =  [
-   {
-      "status":"free",
-      "res":{
-         "staging":[
-            "user1:password123"
-         ]
-      }
-   }
-]
-
-sample_output2 =  [
-   {
-      "status":"busy",
-      "res":{
-         "production":[
-            "user3:password321"
-         ]
-      }
-   }
-]
-
-sample_output3 =  [
-   {
-      "status":"NA",
-      "res":{
-         "staging":[
-            "user2:password321",
-            "user1:password321"
-         ]
-      }
-   }
-]
 
 @hug.startup()
 def get_json_data(api):
@@ -48,31 +15,13 @@ def get_json_data(api):
         JSON_DATA = json.loads(data_file.read())
 
 @hug.get()
-def lock(res, num:hug.types.number=1):
+def lock(res_type, res_value, num:hug.types.number=1):
     """Lock a number of resources of certain type!"""
-    if num == 3:
-        return sample_output3
-    elif num == 2:
-        return sample_output2
-    elif num == 1:
-        return sample_output1
-    else:
-        return JSON_DATA
+    return lock_credentials(JSON_DATA, res_type, res_value)
 
 @hug.post()
 def unlock(body):
     """Unlock resource(s) in json format!"""
-    return body
-
-def is_json_valid(inputjson):
-    try:
-        json_object = json.loads(inputjson)
-    except ValueError:
-        return False
-    return True
-
-def json_to_dict(inputjson):
-    return json.loads(inputjson)
-
-def dict_to_json(inputdict):
-    return json.dumps(inputdict)
+    res_type = next(iter(body))
+    res_value = next(iter(body[res_type]))
+    return unlock_credentials(JSON_DATA, res_type, res_value)
